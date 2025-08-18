6d888cd58dae5e331d6d1e14d76a508dd4c6a3d4
AntthonyQQ <aquiangquiros2025fwd@gmail.com>
2025-08-18 15:10:13 -0600

    funcionalidades de js para seleccionar casilla

---
+++ src/js/funcionalidades.js
@@ -0,0 +1,220 @@
+
+
+
+
+const matrizGame =
+[
+    [, , ],
+    [, , ],
+    [, , ] 
+]
+
+console.log(matrizGame)
+
+//console.log(matrizGame[2][2])
+
+let player = "X"
+let gameTurn = 0;
+
+
+
+
+const btnBox0 = document.getElementById("btnBox0");
+btnBox0.addEventListener("click", function()
+{
+    const position = validarPosicion (btnBox0, 0, 0)
+    console.log(matrizGame)
+
+    if(position == true)
+    {
+        putPosition(0,0)
+    }
+    else
+    {
+        
+    }
+
+})
+
+const btnBox1 = document.getElementById("btnBox1");
+btnBox1.addEventListener("click", function()
+{
+    const position = validarPosicion (btnBox1, 0, 1 )
+    console.log(matrizGame)
+
+    if(position == true)
+    {
+        putPosition(0,1)
+    }
+    else
+    {
+        
+    }
+
+})
+
+const btnBox2 = document.getElementById("btnBox2");
+btnBox2.addEventListener("click", function()
+{
+    const position = validarPosicion (btnBox2, 0, 2 )
+    console.log(matrizGame)
+
+    if(position == true)
+    {
+        putPosition(0,2)
+    }
+    else
+    {
+        
+    }
+
+})
+
+const btnBox3 = document.getElementById("btnBox3");
+btnBox3.addEventListener("click", function()
+{
+    const position = validarPosicion (btnBox3, 1, 0 )
+    console.log(matrizGame)
+
+    if(position == true)
+    {
+        putPosition(1,0)
+    }
+    else
+    {
+        
+    }
+
+})
+
+const btnBox4 = document.getElementById("btnBox4");
+btnBox4.addEventListener("click", function()
+{
+    const position = validarPosicion (btnBox4, 1, 1 )
+    console.log(matrizGame)
+
+    if(position == true)
+    {
+        putPosition(1,1)
+    }
+    else
+    {
+        
+    }
+
+})
+
+const btnBox5 = document.getElementById("btnBox5");
+btnBox5.addEventListener("click", function()
+{
+    const position = validarPosicion (btnBox5, 1, 2 )
+    console.log(matrizGame)
+
+    if(position == true)
+    {
+        putPosition(1, 2)
+    }
+    else
+    {
+        
+    }
+
+})
+
+const btnBox6 = document.getElementById("btnBox6");
+btnBox6.addEventListener("click", function()
+{
+    const position = validarPosicion (btnBox6, 2, 0 )
+    console.log(matrizGame)
+
+    if(position == true)
+    {
+        putPosition(2, 0)
+    }
+    else
+    {
+        
+    }
+
+})
+
+const btnBox7 = document.getElementById("btnBox7");
+btnBox7.addEventListener("click", function()
+{
+    const position = validarPosicion (btnBox7, 2, 1 )
+    console.log(matrizGame)
+
+    if(position == true)
+    {
+        putPosition(2,1)
+    }
+    else
+    {
+        
+    }
+
+})
+
+const btnBox8 = document.getElementById("btnBox8");
+btnBox8.addEventListener("click", function()
+{
+    const position = validarPosicion (btnBox8, 2, 2 )
+    console.log(matrizGame)
+
+    if(position == true)
+    {
+        putPosition(2,2)
+    }
+    else
+    {
+        
+    }
+
+})
+
+
+function validarPosicion (btnId, colunm, row)
+{
+    let position = "";
+    while(position == false )
+    {
+        if(matrizGame[colunm][row] !== "X" && matrizGame[colunm][row] !== "O")
+        {
+            const player = getPlayer(gameTurn)
+            btnId.textContent = player;
+            position = true;
+            gameTurn += 1;
+            console.log(gameTurn)
+            return position;
+        }
+        else
+        {
+            alert("La posicion ya esta ocupada, intentar otra")
+            position = false;
+            return position;
+        }
+    }
+}
+
+function putPosition(colunm, row,)
+{
+    matrizGame[colunm][row] = player;
+}
+
+
+function getPlayer(gameTurn)
+{
+    if(gameTurn === 0)
+    {
+        player = "X"
+    }
+    else if(gameTurn % 2 === 0)
+    {
+        player = "X"
+    }
+    else
+    {
+        player = "O"
+    };
+    return player;
+}
